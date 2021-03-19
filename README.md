f6940c27430b20b7c09546738ff1c687403e86ea
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-03-18 17:57:52 -0700

    adding README, functional code, need to write the last function for updating employee roles

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# EmployeeTracker
+# Employee Tracker 
+## Jennifer Detmering
+
+## Description-
+### A command line program that uses MySql to keep track of employees, departments, roles, id's, salaries, etc.
+#### [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)] 
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Technologies Used](#technologies_used)
+4. [Credits](#credits)
+5. [License](#license)
+6. [Features](#features)
+7. [Questions and Contributions](#questions_and_contributions)
+8. [Tests](#tests)
+        
+##  Installation-
+
+[Video Demonstration of Application](video link)
+    
+##  Usage- 
+
+![]()
+![]()
+
+
+## Technologies Used-
+npm,JavaScript,JQuery,node.js
+
+## Credits-  
+[https://w3Schools.com]
+
+
+## License-
+
+### [![License: MIT]](https://opensource.org/licenses/MIT) 
+
+## Features-
+helps to track employee's info and work data
+
+## Questions and Contributions-  
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.0-4baaaa.svg)](code_of_conduct.md)
+### GitHub Profile: https://github.com/kodiakshuksan
+### Email: kodiakshuksan@yahoo.com
+
+## Tests- 
+none
--- config/trackEmployees.js
+++ config/trackEmployees.js
@@ -33,11 +33,11 @@ const connection = mysql.createConnection({
     database: 'tracker_db',
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
     start();
-  });
+});
 
 // connection.connect((err) => {
 //     if (err) throw err;
@@ -52,7 +52,7 @@ const start = () => {
             message: 'What would you like to do?',
             choices: [
                 'Add Department',
-                'Add Part',
+                'Add Role',
                 'Add Employee',
                 'View Departments',
                 'View Roles',
@@ -68,25 +68,25 @@ const start = () => {
                     break;
 
 
-                        case 'Add Part':
-                            addPart();
-                            break;
+                case 'Add Role':
+                    addPart();
+                    break;
 
-                        case 'Add Employee':
-                            addEmployee();
-                            break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
 
                 case 'View Departments':
                     viewDepartment();
                     break;
 
-                        case 'View Roles':
-                            viewRoles();
-                            break;
+                case 'View Roles':
+                    viewRoles();
+                    break;
 
-                        case 'View Employees':
-                            viewEmployees();
-                            break;
+                case 'View Employees':
+                    viewEmployees();
+                    break;
 
                 //         case 'Update Employee Roles':
                 //             updateEmployeeRoles();
@@ -110,6 +110,11 @@ const start = () => {
                     name: 'department_name',
                     type: 'input',
                     message: 'What Department do you want to add?',
+                },
+                {
+                    name: 'id',
+                    type: 'input',
+                    message: 'What Department ID do you want to add?',
                 }
             ])
 
@@ -117,144 +122,226 @@ const start = () => {
                 connection.query(
                     'INSERT INTO department SET ?',
                     {
+                        id: answers.id,
                         department_name: answers.department_name,
                     },
-                    
+
                     console.log('department inserted!\n'));
                 //     // starts prompts over again for user to have another choice
-                //connection.connect(start);
-             
+                connection.connect(start);
+
             })
 }
 
 const addPart = () =>
-inquirer
-    .prompt([
-        {
-            name: 'part_title',
-            type: 'input',
-            message: 'What Part/Title/Role do you want to add?',
-        },
-        {
-            name: 'salary',
-            type: 'input',
-            message: 'What is the salary for this title?',
-        },
-        {
-            name: 'department_id',
-            type: 'input',
-            message: 'What is the department_id number?',
-        }
-    ])
-
-    .then((answers) => {
-        connection.query(
-            'INSERT INTO part SET ?',
+    inquirer
+        .prompt([
             {
-                title: answers.part_title,
-                salary: answers.salary,
-                department_id: answers.department_id,
+                name: 'part_title',
+                type: 'input',
+                message: 'What Part/Title/Role do you want to add?',
             },
-            
-            console.log('New part/title/role added!\n'));
-        //     // starts prompts over again for user to have another choice
-        connection.connect(start);
-     
-    })
-
-    const addEmployee = () =>
-inquirer
-    .prompt([
-        {
-            name: 'part_title',
-            type: 'input',
-            message: 'What is your first name?',
-        },
-        {
-            name: 'salary',
-            type: 'input',
-            message: 'What is your last name?',
-        },
-        {
-            name: 'department_id',
-            type: 'input',
-            message: 'What is the part_id number?',
-        },
-        {
-            name: 'manager_id',
-            type: 'input',
-            message: 'What is the manager_id number?',
-        }
-    ])
-
-    .then((answers) => {
-        connection.query(
-            'INSERT INTO employee SET ?',
             {
-                first_name: answers.first_name,
-                last_name: answers.last_name,
-                department_id: answers.department_id,
-                manager_id: answers.manager_id,
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary for this title?',
             },
+            {
+                name: 'department_id',
+                type: 'input',
+                message: 'What is the department_id number?',
+            }
+        ])
+
+        .then((answers) => {
+            connection.query(
+                'INSERT INTO part SET ?',
+                {
+                    title: answers.part_title,
+                    salary: answers.salary,
+                    department_id: answers.department_id,
+                },
+
+                console.log('New part/title/role added!\n'));
+            //     // starts prompts over again for user to have another choice
             
-            console.log('New employee added!\n'));
-        //     // starts prompts over again for user to have another choice
+            connection.connect(start);
+            connection.connect(start);
+
+
+        })
+
+const addEmployee = () =>
+    inquirer
+        .prompt([
+            {
+                name: 'first_name',
+                type: 'input',
+                message: 'What is your first name?',
+            },
+            {
+                name: 'last_name',
+                type: 'input',
+                message: 'What is your last name?',
+            },
+            {
+                name: 'part_id',
+                type: 'input',
+                message: 'What is the part_id number?',
+            },
+            {
+                name: 'manager_id',
+                type: 'input',
+                message: 'What is the manager_id number?',
+            }
+        ])
+
+        .then((answers) => {
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: answers.first_name,
+                    last_name: answers.last_name,
+                    part_id: answers.part_id,
+                    manager_id: answers.manager_id,
+                },
+
+                console.log('New employee added!\n'));
+            //     // starts prompts over again for user to have another choice
+            start();
+        })
+    
+
+
+
+const viewDepartment = () => {
+    console.log('Selecting all department...\n');
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        table.res;
+        
+        //connection.end();
         connection.connect(start);
-     
-    })
 
+    });
 
+};
 
-
-
-
-
-
-
-
-
-    const viewDepartment = () => {
-        console.log('Selecting all department...\n');
-        connection.query('SELECT * FROM department', (err, res) => {
-          if (err) throw err;
-          // Log all results of the SELECT statement
-          console.log(res);
-          table.res;
-          connection.connect(start);
-          //connection.end();
-          
-        });
+const viewRoles = () => {
+    console.log('Selecting all parts...\n');
+    connection.query('SELECT * FROM part', (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        table.res;
+        connection.connect(start);
         
-      };
 
-      const viewRoles = () => {
-        console.log('Selecting all parts...\n');
-        connection.query('SELECT * FROM part', (err, res) => {
-          if (err) throw err;
-          // Log all results of the SELECT statement
-          console.log(res);
-          table.res;
-          connection.connect(start);
-          //connection.end();
-          
-        });
+    });
+
+};
+
+const viewEmployees = () => {
+    console.log('Selecting all employees...\n');
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        table.res;
+        connection.connect(start);
         
-      };
-      
-      const viewEmployees = () => {
-        console.log('Selecting all employees...\n');
-        connection.query('SELECT * FROM employee', (err, res) => {
-          if (err) throw err;
-          // Log all results of the SELECT statement
-          console.log(res);
-          table.res;
-          connection.connect(start);
-          //connection.end();
-          
-        });
+
+    });
+
+};
+
+
+    
+
+    //  const updateEmployeeRoles = () => {
+    //     console.log('Updating employee roles...\n');
+    //     const query = connection.query(
+    //       'UPDATE employee SET ? WHERE ?',
+    //       [
+    //         {
+    //           part_id: '' ,
+    //         },
+    //         {
+    //           manager_id: '',
+    //         },
+    //         {
+        //      manager_id: '',
+        //      },
+    //       ],
+    //       (err, res) => {
+    //         if (err) throw err;
+    //         console.log(`${res.affectedRows} Employee Roles updated!\n`);
+    //         // Call deleteProduct AFTER the UPDATE completes
+    //         //deleteProduct();
+    //         const viewEmployees = () => {
+    //         console.log('Selecting all employees...\n');
+    //         connection.query('SELECT * FROM employee', (err, res) => {
+    //         if (err) throw err;
+    //         // Log all results of the SELECT statement
+    //         console.log(res);
+    //         table.res;
+    //         connection.connect(start);
+    //         //connection.end();
+    
+    //     });
+    
+    // };
+    //delete ?
+    // const deleteProduct = () => {
+    //     console.log('Deleting...\n');
+    //     connection.query(
+    //       'DELETE FROM products WHERE ?',
+    //       {
+    //         flavor: 'strawberry',
+    //       },
+    //       (err, res) => {
+    //         if (err) throw err;
+    //         console.log(`${res.affectedRows} deleted!\n`);
+    //         // Call readProducts AFTER the DELETE completes
+    //         readProducts();
+    //       }
+    //     );
+    //   };
+
+//readEmployees and  view employees function
+// const readEmployees = () => {
+//     console.log('Selecting all employees...\n');
+//     connection.query('SELECT * FROM employee', (err, res) => {
+//       if (err) throw err;
+//       // Log all results of the SELECT statement
+//       console.log(res);
+//       connection.end();
+//     });
+//   };
         
-      };
-      
+
+
+
+
+    // -- show ALL books with authors
+    // -- INNER JOIN will only return all matching values from both tables
+    // SELECT title, firstName, lastName
+    // FROM books
+    // INNER JOIN authors ON books.authorId = authors.id;
+    
+    // -- show ALL books, even if we don't know the author
+    // -- LEFT JOIN returns all of the values from the left table, and the matching ones from the right table
+    // SELECT title, firstName, lastName
+    // FROM books
+    // LEFT JOIN authors ON books.authorId = authors.id;
+    
+    // -- show ALL books, even if we don't know the author
+    // -- RIGHT JOIN returns all of the values from the right table, and the matching ones from the left table
+    // SELECT title, firstName, lastName
+    // FROM books
+    // RIGHT JOIN authors ON books.authorId = authors.id;
 
 
 
@@ -272,9 +359,6 @@ inquirer
 
 
 
-
-
-              
 
 
 
